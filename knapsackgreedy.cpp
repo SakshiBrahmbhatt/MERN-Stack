a6db19ec0a71ca555c49a344528abd55463eb768
Sakshi@2003 <87719705+SakshiBrahmbhatt@users.noreply.github.com>
2022-08-30 12:43:18 +0530

    initial commit

---
+++ knapsackgreedy.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define Max 10
+#define W 16
+class Knapsack{
+    int n,i,j;
+    float d[Max],w[Max],v[Max],temp;
+    public:
+    Knapsack()
+    {
+        int cw=0;
+        float m=0,k=0;
+        cout<<"How many no. you want to enter?:";
+        cin>>n;
+        cout<<"Enter "<<n<<" elements weight one by one"<<endl;
+        for(i=0;i<n;i++)
+        cin>>w[i];
+        cout<<endl<<"Enter "<<n<<" element value one by one"<<endl;
+        for(i=0;i<n;i++)
+        cin>>v[i];
+        for(i=0;i<n;i++)
+        d[i]=(float)(v[i]/w[i]);
+        //Arranging according to density
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n-1;j++)
+            {
+                if(d[i]>d[j])
+                {
+                    temp=d[i];
+                    d[i]=d[j];
+                    d[j]=temp;
+                    temp=w[i];
+                    w[i]=w[j];
+                    w[j]=temp;
+                    temp=v[i];
+                    v[i]=v[j];
+                    v[j]=temp;
+                }
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            cout<<w[i]<<"\t"<<v[i]<<"\t"<<(float)(d[i])<<endl;
+        }
+        for(i=0;i<n;i++)
+        {
+            if(cw+w[i]<=W)
+            {
+                cw+=w[i];
+                m+=v[i];
+            }
+            else if(cw!=W)
+            {
+                k=W-cw;
+                m+=(k*d[i]);
+            }
+            else 
+            break;
+        }
+        cout<<endl<<"Total profit = "<<m;
+    }
+};
+int main()
+{
+    Knapsack k;
+    return 0;
+}
